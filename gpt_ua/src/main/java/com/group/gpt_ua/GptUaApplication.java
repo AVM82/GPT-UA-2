2a1dd72a361db0562c11005b2ce81647ca5355d4
sn <shurick2211@gmail.com>
2023-01-31 21:54:48 +0200

    3

---
+++ gpt_ua/src/main/java/com/group/gpt_ua/GptUaApplication.java
@@ -0,0 +1,13 @@
+package com.group.gpt_ua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptUaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GptUaApplication.class, args);
+  }
+
+}
