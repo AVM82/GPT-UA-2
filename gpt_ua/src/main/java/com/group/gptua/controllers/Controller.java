558dcf5d14b4acc439fe1bbd8c98abeeb1e2c663
Maxim Olkhovskiy <olkhovskiy.nkmz@gmail.com>
2023-02-02 12:15:59 +0200

    add swagger

--- gpt_ua/src/main/java/com/group/gptua/controllers/Controller.java
+++ gpt_ua/src/main/java/com/group/gptua/controllers/Controller.java
@@ -1,6 +1,10 @@
 package com.group.gptua.controllers;
 
 import com.group.gptua.bot.Bot;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@OpenAPIDefinition(info = @Info(title = "GPT-UA",
+    version = "${api.ver}",
+    description = "This is the application provides GPT-chat access..."))
+@Tag(name = "Controller API", description = "This controller manages the GPT chat...")
 @RequestMapping("/bot")
 @Slf4j
 public class Controller {
@@ -22,6 +30,7 @@ public class Controller {
    * @return string
    */
   @GetMapping
+  @Operation(summary = "getEcho-method", description = "this method tests controller")
   public ResponseEntity<?> index(@RequestParam(name = "mess", required = false) String message) {
     log.info("Message: {} ",message);
     if (Bot.chatIDLast != null) {
