bb5b2503149fd641a282eed2cfb0f59351c244f6
sn <shurick2211@gmail.com>
2023-02-07 22:24:31 +0200

    change front style & userDto

--- front/src/main/angular/src/app/services/user.services.ts
+++ front/src/main/angular/src/app/services/user.services.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {DtoMess} from "../../dto/dto.mess";
 import {DtoUser} from "../../dto/dto.user";
 import {environment} from "../../environments/environment";
 
