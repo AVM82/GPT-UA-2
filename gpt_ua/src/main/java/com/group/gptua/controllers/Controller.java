4f6e66e9ce02b1d71e7021bf55c0fa99bf4f3b1f
Shurick2211 <47493485+Shurick2211@users.noreply.github.com>
2023-02-08 20:29:15 +0200

    Update angular (#10)
    * update angular & devInfo
    
    * fix style
    
    * update front version angular
    
    * update version angular

--- gpt_ua/src/main/java/com/group/gptua/controllers/Controller.java
+++ gpt_ua/src/main/java/com/group/gptua/controllers/Controller.java
@@ -11,15 +11,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,8 +59,10 @@ public class Controller {
    */
   @GetMapping
   @Operation(summary = "getEcho-method", description = "this method tests controller")
-  public ResponseEntity<?> index(@RequestParam(name = "mess", required = false) String message) {
+  public ResponseEntity<?> index(@RequestParam(name = "mess", required = false) String message,
+      HttpServletRequest request) {
     log.info("Message: {} ", message);
+    log.info("REQUST: {} ", request.getHeader("referer"));
     return ResponseEntity.ok(new DtoMessage(openAi.getTextMessage(Models.ADA,message)));
   }
 
