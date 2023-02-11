aab67d76b31526afa1e91f2df73b3962a21a6275
Oleksandr Hrabovets <alex6754035@gmail.com>
2023-02-11 16:47:24 +0200

    GPT-15: change GptMessageServiceTest +logger

--- gpt_ua/src/test/com/group/gptua/service/GptMessageServiceTest.java
+++ gpt_ua/src/test/com/group/gptua/service/GptMessageServiceTest.java
@@ -3,23 +3,29 @@ package com.group.gptua.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.group.gptua.dto.DtoMessage;
 import com.group.gptua.utils.Models;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class GptMessageServiceTest {
 
   private final GptMessageService gptMessageService = mock(GptMessageService.class);
+  private final String userHash = "testUserHash";
+  private final String message = "test message";
+  private final Models model = Models.ADA;
+  private final DtoMessage dtoMessage = new DtoMessage(message, model);
+  private static final Logger log = LoggerFactory.getLogger(GptMessageServiceTest.class);
 
   @Test
   void getAnswer() {
-    String userHash = "testUserHash";
-    String message = "test message";
-    Models model = Models.ADA;
-    DtoMessage dtoMessage = new DtoMessage(message, model);
 
+    log.info("start test getAnswer");
     when(gptMessageService.getAnswer(any(), any()))
         .thenAnswer(invocation -> {
           String invUserHash = invocation.getArgument(0);
@@ -32,4 +38,14 @@ class GptMessageServiceTest {
 
     assertEquals(dtoMessage, gptMessageService.getAnswer(userHash, dtoMessage));
   }
+
+  @Test
+  void getAnswerVerify() {
+
+    log.info("start test getAnswerVerify");
+    gptMessageService.getAnswer(userHash, dtoMessage);
+    verify(gptMessageService, times(1)).getAnswer(userHash, dtoMessage);
+
+  }
+
 }
