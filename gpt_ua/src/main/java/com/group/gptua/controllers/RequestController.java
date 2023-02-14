fe57c91d2a8d2994828f0cb34104e0ad4e38482a
Maxim Olkhovskiy <olkhovskiy.nkmz@gmail.com>
2023-02-15 01:57:25 +0200

    archive+two dates+checkstyle

--- gpt_ua/src/main/java/com/group/gptua/controllers/RequestController.java
+++ gpt_ua/src/main/java/com/group/gptua/controllers/RequestController.java
@@ -55,13 +55,13 @@ public class RequestController {
         || filter.getText().equals("")
         || request.getRequest().contains(filter.getText());
     boolean date = (filter.getDateFrom() == null && filter.getDateTo() == null)
-        || (filter.getDateFrom() != null &&
-        request.getCreatedAt().toLocalDate().isEqual(filter.getDateFrom()))
-        || (filter.getDateTo() != null &&
-            request.getCreatedAt().toLocalDate().isEqual(filter.getDateTo()))
-        || (filter.getDateFrom() != null && filter.getDateTo() != null)&&
-        (request.getCreatedAt().toLocalDate().isAfter(filter.getDateFrom())&&
-        request.getCreatedAt().toLocalDate().isBefore(filter.getDateTo()));
+        || (filter.getDateFrom() != null
+        && request.getCreatedAt().toLocalDate().isEqual(filter.getDateFrom()))
+        || (filter.getDateTo() != null
+        && request.getCreatedAt().toLocalDate().isEqual(filter.getDateTo()))
+        || (filter.getDateFrom() != null && filter.getDateTo() != null)
+        && (request.getCreatedAt().toLocalDate().isAfter(filter.getDateFrom())
+        && request.getCreatedAt().toLocalDate().isBefore(filter.getDateTo()));
     return user && model && text && date;
   }
 
