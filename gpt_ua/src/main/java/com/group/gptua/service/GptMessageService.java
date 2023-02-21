e70dd18a98f707e202fca1e48ed2bdde1b60b58c
OleksandrHrabovets <115089978+OleksandrHrabovets@users.noreply.github.com>
2023-02-21 15:31:49 +0200

    GPT-52: add actuator, prometheus endpoint, metrics for request by model, change gitflow for versions (#62)

--- gpt_ua/src/main/java/com/group/gptua/service/GptMessageService.java
+++ gpt_ua/src/main/java/com/group/gptua/service/GptMessageService.java
@@ -4,6 +4,12 @@ import com.group.gptua.dto.DtoMessage;
 import com.group.gptua.model.UserRequestEntity;
 import com.group.gptua.utils.Models;
 import com.group.gptua.utils.NoFreeTokenException;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -17,16 +23,32 @@ public class GptMessageService implements GptMessageServiceInt {
   private final UserRequestService userRequestService;
   private final UserSessionServiceInt userSessionService;
   public static final String EXCEPTION_MESSAGE = "Спробуйте пізніше, або виберіть іншу модель";
+  private final MeterRegistry meterRegistry;
+  private final Map<Models, Counter> modelRequestCounters = new EnumMap<>(Models.class);
 
   /**
    * Constructor.
    */
   public GptMessageService(UserRequestService userRequestService,
       @Qualifier("openAiService") OpenAiInt openAiClient,
-      @Qualifier("userSessionService") UserSessionServiceInt userSessionService) {
+      @Qualifier("userSessionService") UserSessionServiceInt userSessionService,
+      MeterRegistry meterRegistry) {
     this.userRequestService = userRequestService;
     this.openAiClient = openAiClient;
     this.userSessionService = userSessionService;
+    this.meterRegistry = meterRegistry;
+  }
+
+  /**
+   * Post construct init method.
+   */
+  @PostConstruct
+  public void init() {
+    Arrays.asList(Models.values()).forEach(m ->
+        modelRequestCounters.put(m,
+            Counter.builder("request_by_model_" + m.name())
+                .description("number of request by model " + m.name())
+                .register(meterRegistry)));
   }
 
   /**
@@ -50,6 +72,7 @@ public class GptMessageService implements GptMessageServiceInt {
       log.warn("exception on getAnswer(): {} {}", e.getMessage(), e.getStackTrace());
       return new DtoMessage(EXCEPTION_MESSAGE, model);
     }
+    modelRequestCounters.get(model).increment();
     if (!userHash.isEmpty()) {
       saveRequest(model, userHash, request, response);
     }
